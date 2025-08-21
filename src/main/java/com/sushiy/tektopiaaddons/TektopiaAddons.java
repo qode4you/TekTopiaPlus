acb4657b2cbdaaff9f28cd634e2d02bd860dec33
Jakob <42284544+qode4you@users.noreply.github.com>
2025-08-21 16:14:58 +0200

    Updated Modinfo
    Changed name, id and version of TekTopiaPlus (1.0.0)

--- src/main/java/com/sushiy/tektopiaaddons/TektopiaAddons.java
+++ src/main/java/com/sushiy/tektopiaaddons/TektopiaAddons.java
@@ -28,9 +28,9 @@ import java.util.*;
 	version = TektopiaAddons.VERSION
 )
 public class TektopiaAddons {
-	public static final String MODID = "tektopiaaddons";
-	public static final String NAME = "Tekotpia Addons";
-	public static final String VERSION = "1.5.1";
+	public static final String MODID = "tektopiaplus";
+	public static final String NAME = "TekTopiaPlus";
+	public static final String VERSION = "1.0.0";
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
