38a3f0bcb6f1688a810be7759e0756d42eb4de0a
XMxmx111 <740057300@qq.com>
2026-01-03 23:45:06 +0800

    Rewrite tree-cutting algorithm using BFS

--- src/main/java/com/sushiy/tektopiaaddons/mixin/EntityAIChopTreeMixin.java
+++ src/main/java/com/sushiy/tektopiaaddons/mixin/EntityAIChopTreeMixin.java
@@ -2,6 +2,7 @@ package com.sushiy.tektopiaaddons.mixin;
 
 import com.sushiy.tektopiaaddons.IOreFinderVillage;
 import com.sushiy.tektopiaaddons.oredictfinder.TreeOreScanner;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockNewLog;
 import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.BlockPlanks;
@@ -20,7 +21,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.List;
+import java.util.*;
 
 @Mixin(value = EntityAIChopTree.class)
 public abstract class EntityAIChopTreeMixin extends EntityAIMoveToBlock {
@@ -40,12 +41,12 @@ public abstract class EntityAIChopTreeMixin extends EntityAIMoveToBlock {
     @Shadow(remap = false)
     private void releaseTreeClaim(){}
     @Shadow(remap = false)
-    private int chopTreeOak(BlockPos treePos) {
-        return 0;
+    private boolean logDropCheck(int skill) {
+        return false;
     }
     @Shadow(remap = false)
-    private int chopTreeStraight(BlockPos treePos) {
-        return 0;
+    private boolean chopLog(BlockPos pos, boolean dropBlock, boolean adjacentLeafCheck) {
+        return false;
     }
 
     protected BlockPos getDestinationBlock() {
@@ -58,11 +59,8 @@ public abstract class EntityAIChopTreeMixin extends EntityAIMoveToBlock {
                 if (blockState.getBlock() == Blocks.LOG) {
                     filterName = "chop_tree_" + blockState.getValue(BlockOldLog.VARIANT).getName();
                 }
-                else if (blockState.getBlock() == Blocks.LOG2) {
-                    filterName = "chop_tree_" + blockState.getValue(BlockNewLog.VARIANT).getName();
-                }
                 else {
-                    filterName = "chop_tree_modded";
+                    filterName = "chop_tree_" + blockState.getValue(BlockNewLog.VARIANT).getName();
                 }
 
                 if (this.villager.isAIFilterEnabled(filterName)) {
@@ -91,17 +89,7 @@ public abstract class EntityAIChopTreeMixin extends EntityAIMoveToBlock {
                 this.treeType = blockState.getValue(BlockNewLog.VARIANT);
             }
 
-            switch (this.treeType) {
-                case OAK:
-                    logsChopped = this.chopTreeOak(this.treePos);
-                    break;
-                case BIRCH:
-                case JUNGLE:
-                case SPRUCE:
-                default:
-                    logsChopped = this.chopTreeStraight(this.treePos);
-                    break;
-            }
+            logsChopped = this.chopTreeUniversal(this.treePos);
 
             if (logsChopped > 0) {
                 this.villager.throttledSadness(-2);
@@ -111,6 +99,150 @@ public abstract class EntityAIChopTreeMixin extends EntityAIMoveToBlock {
             }
         }
 
+    }
+
+    private static final BlockPos[][] DIR_4 = {
+            {new BlockPos(-1, 0, 0), new BlockPos(1, 0, 0)},
+            {new BlockPos(0, 0, -1), new BlockPos(0, 0, 1)}
+    };
+
+    private static final BlockPos[][] DIR_8 = {
+            {new BlockPos(-1, 0, 0), new BlockPos(1, 0, 0)},
+            {new BlockPos(0, 0, -1), new BlockPos(0, 0, 1)},
+            {new BlockPos(-1, 0, -1), new BlockPos(1, 0, 1)},
+            {new BlockPos(-1, 0, 1), new BlockPos(1, 0, -1)}
+    };
+
+    private int chopTreeUniversal(BlockPos treePos) {
+        int count = 0;
+        Block originalLog = this.villager.world.getBlockState(treePos).getBlock();
+        int currentY = treePos.getY();
+        Map<Integer, Set<BlockPos>> logsByLayer = new TreeMap<>();
+        Set<BlockPos> currentLayerLogs = new HashSet<>();
+        Set<BlockPos> visited = new HashSet<>();
+        Set<BlockPos> confirmed = new HashSet<>();
+        Queue<BlockPos> currentLayerQueue = new ArrayDeque<>();
+        currentLayerQueue.add(treePos);
+        confirmed.add(treePos);
+        visited.add(treePos);
+        currentLayerLogs.add(treePos);
+        while (!currentLayerQueue.isEmpty()) {
+            BlockPos current = currentLayerQueue.poll();
+            for (BlockPos[] dirs : DIR_4) {
+                for (BlockPos dir : dirs) {
+                    BlockPos neighbor = current.add(dir);
+                    if (!visited.contains(neighbor)) {
+                        visited.add(neighbor);
+                        if ((confirmed.contains(neighbor) || this.villager.world.getBlockState(neighbor).getBlock() == originalLog) && confirmLogLine(neighbor, originalLog, visited, confirmed)) {
+                            currentLayerQueue.add(neighbor);
+                            confirmed.add(neighbor);
+                            currentLayerLogs.add(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+        logsByLayer.put(currentY, currentLayerLogs);
+        currentY++;
+
+        Set<BlockPos> nextLayerLogs = new HashSet<>();
+        for (BlockPos logPos : currentLayerLogs) {
+            BlockPos above = logPos.up();
+            if (confirmed.contains(above) || this.villager.world.getBlockState(above).getBlock() == originalLog) {
+                currentLayerQueue.add(above);
+                confirmed.add(above);
+                visited.add(above);
+                nextLayerLogs.add(above);
+            }
+            else {
+                for (BlockPos[] dirs : DIR_8) {
+                    for (BlockPos dir : dirs) {
+                        BlockPos neighbor = above.add(dir);
+                        if (!visited.contains(neighbor)) {
+                            visited.add(neighbor);
+                            if ((confirmed.contains(neighbor) || this.villager.world.getBlockState(neighbor).getBlock() == originalLog) && this.villager.world.getBlockState(neighbor.down()).getBlock() != originalLog && confirmLogLine(neighbor, originalLog, visited, confirmed)) {
+                                currentLayerQueue.add(neighbor);
+                                confirmed.add(neighbor);
+                                nextLayerLogs.add(neighbor);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        currentLayerLogs = nextLayerLogs;
+        while (!currentLayerQueue.isEmpty()) {
+            BlockPos current = currentLayerQueue.poll();
+            for (BlockPos[] dirs : DIR_8) {
+                for (BlockPos dir : dirs) {
+                    BlockPos neighbor = current.add(dir);
+                    if (!visited.contains(neighbor)) {
+                        visited.add(neighbor);
+                        if ((confirmed.contains(neighbor) || this.villager.world.getBlockState(neighbor).getBlock() == originalLog) && confirmLogLine(neighbor, originalLog, visited, confirmed)) {
+                            currentLayerQueue.add(neighbor);
+                            confirmed.add(neighbor);
+                            currentLayerLogs.add(neighbor);
+                        }
+                    }
+                }
+            }
+            logsByLayer.put(currentY, currentLayerLogs);
+            currentY++;
+
+            nextLayerLogs = new HashSet<>();
+            for (BlockPos logPos : currentLayerLogs) {
+                BlockPos above = logPos.up();
+                if (confirmed.contains(above) || this.villager.world.getBlockState(above).getBlock() == originalLog) {
+                    currentLayerQueue.add(above);
+                    confirmed.add(above);
+                    visited.add(above);
+                    nextLayerLogs.add(above);
+                }
+                else {
+                    for (BlockPos[] dirs : DIR_8) {
+                        for (BlockPos dir : dirs) {
+                            BlockPos neighbor = above.add(dir);
+                            if (!visited.contains(neighbor)) {
+                                visited.add(neighbor);
+                                if ((confirmed.contains(neighbor) || this.villager.world.getBlockState(neighbor).getBlock() == originalLog) && !confirmed.contains(neighbor.down()) && this.villager.world.getBlockState(neighbor.down()).getBlock() != originalLog && confirmLogLine(neighbor, originalLog, visited, confirmed)) {
+                                    currentLayerQueue.add(neighbor);
+                                    confirmed.add(neighbor);
+                                    nextLayerLogs.add(neighbor);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            currentLayerLogs = nextLayerLogs;
+        }
+
+        int skill = this.villager.getSkill(ProfessionType.LUMBERJACK);
+        for (Integer layerY : logsByLayer.keySet()) {
+            for (BlockPos logPos : logsByLayer.get(layerY)) {
+                if (this.chopLog(logPos, count == 0 || this.logDropCheck(skill), false)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private boolean confirmLogLine(BlockPos neighbor, Block originalLog, Set<BlockPos> visited, Set<BlockPos> confirmed) {
+        int horizontalLength = 1;
+        for (BlockPos[] dirs : DIR_8) {
+            for (BlockPos dir : dirs) {
+                BlockPos neighborPos = neighbor;
+                while (horizontalLength < 5 && (!visited.contains(neighborPos.add(dir)) || neighborPos.add(dir).equals(this.treePos)) && (confirmed.contains(neighborPos.add(dir)) || this.villager.world.getBlockState(neighborPos.add(dir)).getBlock() == originalLog)) {
+                    horizontalLength++;
+                    neighborPos = neighborPos.add(dir);
+                    confirmed.add(neighborPos);
+                }
+            }
+            horizontalLength = horizontalLength < 5 ? 1 : horizontalLength;
+        }
+        return horizontalLength < 5;
     }
 
     /**
